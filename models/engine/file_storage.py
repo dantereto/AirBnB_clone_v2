64e0ac9966327bbc9a87f86fc727e2e21a26985f
JuanDAC <juanavila12.0308@gmail.com>
2021-08-09 13:02:22 -0400

    [fix] (remake other delete and all methods)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,10 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if not cls:
+        """if not cls:
             return FileStorage.__objects
         items = self.__objects.items()
-        return {key: obj for (key, obj) in items if isinstance(obj, cls)}
+        return {key: obj for (key, obj) in items if isinstance(obj, cls)}"""
+        if cls:
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, cls)}
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
