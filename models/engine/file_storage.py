d9534da2c26ca61af9301ee602cf5cbd95f6a553
dantereto <2247@holbertonschool.com>
2021-08-09 02:06:03 +0000

    correct changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+
 import json
 
 
@@ -55,6 +56,7 @@ class FileStorage:
             pass
     def delete(self, obj=None):
         if obj:
-            key = obj.__class.__name + '.' + obj.id
-            del self.__objects[key]
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
         self.save()
