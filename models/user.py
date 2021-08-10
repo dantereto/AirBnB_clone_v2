20058e635f2af686d5114752e35028e4d2dc7e92
dantereto <2247@holbertonschool.com>
2021-08-10 21:58:51 +0000

    correct atrubites

--- models/user.py
+++ models/user.py
@@ -5,13 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user",
                           cascade="all, delete-orphan")
     reviews = relationship("Review", backref="user",
