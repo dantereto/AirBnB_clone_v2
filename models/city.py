f99e4caf99e5c8edf2e4dbcfa231c42320a0126d
dantereto <2247@holbertonschool.com>
2021-08-10 20:47:25 +0000

    all the subclasses

--- models/city.py
+++ models/city.py
@@ -4,10 +4,12 @@ from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    cities = relationship("City", backref="state",
+    places = relationship("Place", backref="cities",
                           cascade="all, delete-orphan")
--- models/state.py
+++ models/state.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 class State(BaseModel):
     """ State class """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+    @property
+    def cities(self):
+        list_c = []
+        for city in storage.models.all(City):
+            if city.state_id == self.id:
+                list_c.append(city)
+        return (list_c)
