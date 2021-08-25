c52e6a0fbf04c4536b31060c902837eeac823e48
dantereto <2247@holbertonschool.com>
2021-08-25 20:36:18 +0000

    error

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from the contents of the web_static"""
+from datetime import datetime
+import os.path
+from fabric.api import local
+
+
+def do_pack():
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    path = 'versions/web_static_{}.tgz'.format(time)
+    local('tar -cvzf' + path + ' web_static')
+    if os.path.exists(path):
+        return path
+    else:
+        return None
+
+def do_deploy(archive_path):
+    if (os.path.exists('archive_path') is False):
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        route = archive_path.split('/')[-1]
+        folder = ('/data/web_static/releases/' + route-split('.')[0])
+        run('mkdir -p archive_path')
+        run('tar -cvzf /tmp/{} -C {}'.format(route, folder))
+        run('rm /tmp/' + route)
+        run('rm /data/web_static/current')
+        run('ln -s' + folder + '/data/web_static/current')
+        return (True)
+    except:
+        return (False)
