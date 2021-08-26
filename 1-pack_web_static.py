2084caaba481de03bd57e735084ebea745c30b32
dantereto <2247@holbertonschool.com>
2021-08-26 12:38:35 -0500

    correct

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,7 @@ def do_pack():
     """start"""
     time = datetime.now().strftime('%Y%m%d%H%M%S')
     path = 'versions/web_static_{}.tgz'.format(time)
-    if os.path.isdir("versions") is False:
-        local('mkdir -p versions')
+    local('mkdir -p versions')
     local('tar -cvzf' + path + ' web_static')
     if os.path.exists(path):
         return path
