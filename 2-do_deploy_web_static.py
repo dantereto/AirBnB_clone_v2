df4a3731dd7a0e38bf2a1688b48ef32ff005ac11
dantereto <2247@holbertonschool.com>
2021-09-09 09:45:45 -0500

    c is not cool

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,24 +19,22 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ deploy """
-    if (os.path.isfile(archive_path) is False):
+    """distributes an archive to your web servers"""
+    if not os.path.exists(archive_path):
         return False
-
     try:
-
-        route = archive_path.split('/')[-1]
-        folder = ('/data/web_static/releases/' + route.split('.')[0])
-        put(archive_path, '/tmp/' + route)
-        run('sudo mkdir -p {}'.format(folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.
-            format(route, folder))
-        run('sudo rm /tmp/{}'.format(route))
-        run('sudo mv {}/web_static/* {}/'.format(folder, folder))
-        run('sudo rm -rf {}/web_static'.format(folder))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(folder))
-        print("New version deployed!")
+        dire = "data/web_static/releases"
+        file_name = archive_path.split('/')[-1]
+        wext = file_name.split('.')[0]
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("sudo mkdir -p mkdir -p /{}/{}".format(dire, wext))
+        run("sudo tar -xzf /tmp/{} -C /{}/{}/".format(file_name, dire, wext))
+        run("sudo rm /tmp/{}".format(file_name))
+        run("sudo mv /{}/{}/web_static/* /{}/{}/".format(dire, wext, dire, wext))
+        run("sudo rm -rf /{}/{}/web_static".format(dire, wext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /{}/{}/ /data/web_static/current".format(dire, wext))
         return True
-    except:
+
+    except Exception:
         return False
