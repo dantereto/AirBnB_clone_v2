68d79d609e951dcb84baf679eff5ed02c804cf17
dantereto <2247@holbertonschool.com>
2021-09-11 17:44:18 +0000

    correct

--- models/state.py
+++ models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import Base, BaseModel
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -20,7 +22,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
+            """get a list of the city"""
             city_list = []
             for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
